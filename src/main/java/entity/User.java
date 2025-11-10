ac4f6378ed2b63d0d20c816d2b107093fe57592f
HDEnt <dudoeverything@gmail.com>
2025-11-10 13:05:42 -0500

    initial directories

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,8 @@
+package entity;
+
+public Class User {
+    private final String username;
+    private final String password;
+    private final String bio;
+    private final String
+}
---
+++ src/main/java/use_case/make_post/case4.java
@@ -0,0 +1,1 @@
+package main.java.use_case.case4;
