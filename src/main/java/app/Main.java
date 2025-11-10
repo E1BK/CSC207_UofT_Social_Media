1fc3443aeea8cc391f39bb906a6afecff53c648e
E1BK <ibekur@gmail.com>
2025-11-10 12:00:56 -0500

    Setting up the starting Files that we will need.

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,17 @@
+package app;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        AppBuilder appBuilder = new AppBuilder();
+
+        "Under JFrame application we initiate the GUI page i.e. .addLoginView()"
+        JFrame application = appBuilder
+
+
+        application.pack();
+        application.setLocationRelativeTo(null);
+        application.setVisible(true);
+    }
+}
