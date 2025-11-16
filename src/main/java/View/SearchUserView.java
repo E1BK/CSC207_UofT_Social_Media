bcfd60dc6031e6261e1b0a469f29840a2787952d
Hasan Malik <hasanimranmalik245@gmail.com>
2025-11-16 02:00:57 -0500

    designed the basic ui for the SearchUser page

--- src/main/java/View/SearchUserView.java
+++ src/main/java/View/SearchUserView.java
@@ -1,9 +1,13 @@
 package View;
 
+import app.GradientPanel;
+import interface_adapter.landing.LandingViewModel;
 import interface_adapter.searchUser.SearchUserController;
 import interface_adapter.searchUser.SearchUserViewModel;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
@@ -15,13 +19,83 @@ public class SearchUserView extends JPanel implements ActionListener, PropertyCh
     private SearchUserViewModel searchUserViewModel;
     private SearchUserController searchUserController = null;
 
+    private final JButton me;
+    private final JButton people;
+    private final JButton posts;
+
     public SearchUserView(SearchUserViewModel searchUserViewModel) {
         this.searchUserViewModel = searchUserViewModel;
         this.searchUserViewModel.addPropertyChangeListener(this);
 
-        JLabel title = new JLabel("Search for your friends at UofT:");
-        this.add(title);
+        // top panel
+        JLabel name = new JLabel("UofTeam");
+        name.setFont(new Font("Helvetica", Font.PLAIN, 30));
+        GradientPanel topPanel = new GradientPanel();
+        topPanel.add(name);
+        topPanel.setBorder(new EmptyBorder(15, 0, 15, 0));
+
+        // middle panel
+        JPanel middlePanel = new JPanel();
+        middlePanel.setLayout(new BorderLayout());
+
+        JLabel title = new JLabel("Find your friends here!");
+        JPanel titlePanel = new JPanel();
+        titlePanel.add(title);
+
+        title.setFont(new Font("Helvetica", Font.BOLD, 40));
+        JTextField searchBar = new JTextField(20);
+        JLabel searchPrompt = new JLabel("Your friend's username:");
+        searchPrompt.setFont(new Font("Helvetica", Font.BOLD, 20));
+        searchBar.setFont(new Font("Helvetica", Font.PLAIN, 20));
+        JButton searchButton = new JButton("Search");
+        searchButton.setFont(new Font("Helvetica", Font.BOLD, 20));
+        LabelTextPanel searchBarPanel = new LabelTextPanel(searchPrompt, searchBar, searchButton);
+
+        titlePanel.setBorder(new EmptyBorder(5, 0, 5, 0));
+        searchBarPanel.setBorder(new EmptyBorder(5, 0, 5, 0));
+
+        middlePanel.add(titlePanel, BorderLayout.NORTH);
+        middlePanel.add(searchBarPanel, BorderLayout.CENTER);
+
+        // middlePanel.add(displayResultPanel, BorderLayout.SOUTH)
+        // NOTE: when the searching functionality has been implemented.
+        // then use the displayResultPanel to display the resulting posts
+        // of the searched user, or to display the error message if the user does not exist.
+
+        // bottom panel
+        GradientPanel bottomPanel = new GradientPanel();
+        me = new JButton(LandingViewModel.ME_BUTTON_LABEL);
+        me.setFont(new Font("Helvetica", Font.BOLD, 15));
+        people = new JButton(LandingViewModel.PEOPLE_BUTTON_LABEL);
+        people.setFont(new Font("Helvetica", Font.BOLD, 15));
+        posts = new JButton(LandingViewModel.POSTS_BUTTON_LABEL);
+        posts.setFont(new Font("Helvetica", Font.BOLD, 15));
+
+        me.setMargin(new Insets(10, 20, 10, 20));
+        posts.setMargin(new Insets(10, 20, 10, 20));
+        people.setMargin(new Insets(10, 20, 10, 20));
+        searchButton.setMargin(new Insets(6, 20, 6, 20));
+        searchBar.setMargin(new Insets(10, 20, 10, 20));
+
+        bottomPanel.add(posts);
+        bottomPanel.add(people);
+        bottomPanel.add(me);
+        bottomPanel.setBorder(new EmptyBorder(15, 0, 15, 0));
+
+        this.setLayout(new BorderLayout());
+        this.add(topPanel, BorderLayout.NORTH);
+        this.add(middlePanel, BorderLayout.CENTER);
+        this.add(bottomPanel, BorderLayout.SOUTH);
     }
+
+
+//    public void paintComponent(Graphics g) {
+//        super.paintComponent(g);
+//        Graphics2D g2d = (Graphics2D) g;
+//        GradientPaint gradientPaint = new GradientPaint(0, 0, new Color(0x80ffdb), getWidth(), getHeight(), Color.CYAN);
+//        g2d.setPaint(gradientPaint);
+//        g2d.fillRect(0, 0, getWidth(), getHeight());
+//    };
 
 
     public String getViewName() {
