2d318bbd087cdcb790c94a58667fa2a2e276ca7d
MikeYang533 <mikee.yang@mail.utoronto.ca>
2025-11-18 14:18:57 -0500

    changes to mike branch

--- src/main/java/data_access/DBUserDataAccessObject.java
+++ src/main/java/data_access/DBUserDataAccessObject.java
@@ -6,7 +6,10 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONArray;
 
-
+import use_case.login_signup.change_passwrod.ChangePasswordUserDataAccessInterface;
+import use_case.login_signup.login.LoginUserDataAccessInterface;
+import use_case.login_signup.logout.LogoutUserDataAccessInterface;
+import use_case.login_signup.signup.SignupUserDataAccessInterface;
 import use_case.make_post.MakePostUserDataAccessInterface;
 import use_case.profile.ProfileUserDataAccessInterface;
 import use_case.search_user.SearchUserDataAccessInterface;
@@ -15,8 +18,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class DBUserDataAccessObject implements MakePostUserDataAccessInterface,
-                                               SearchUserDataAccessInterface,
-                                               ProfileUserDataAccessInterface {
+        SearchUserDataAccessInterface,
+        ChangePasswordUserDataAccessInterface,
+        LoginUserDataAccessInterface,
+        LogoutUserDataAccessInterface,
+        SignupUserDataAccessInterface{
 
     private static final String STATUS_CODE_LABEL = "status_code";
     private static final int SUCCESS_CODE = 200;
@@ -42,11 +48,15 @@ public class DBUserDataAccessObject implements MakePostUserDataAccessInterface,
     private final PostFactory postFactory;
     private final CommentFactory commentFactory;
 
+    private String currentUsername;
+
     public DBUserDataAccessObject(UserFactory userFactory, PostFactory postFactory, CommentFactory commentFactory){
         this.userFactory = userFactory;
         this.postFactory = postFactory;
         this.commentFactory = commentFactory;
+        this.currentUsername = null;
     }
+
 
     @Override
     public void save(User user){
@@ -112,10 +122,60 @@ public class DBUserDataAccessObject implements MakePostUserDataAccessInterface,
         }
     }
 
-    // This method needs to be implemented
     @Override
-    public User findUserByUsername(String username) {
-        return null;
+    public boolean existsByName(String username) {
+        final OkHttpClient client = new OkHttpClient().newBuilder().build();
+        final Request request = new Request.Builder()
+                .url(String.format("http://vm003.teach.cs.toronto.edu:20112/checkIfUserExists?username=%s", username))
+                .addHeader(CONTENT_TYPE_LABEL, CONTENT_TYPE_JSON)
+                .build();
+        try {
+            final Response response = client.newCall(request).execute();
+            final JSONObject responseBody = new JSONObject(response.body().string());
+            return responseBody.getInt(STATUS_CODE_LABEL) == SUCCESS_CODE;
+        }
+        catch (IOException | JSONException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    @Override
+    public User get(String username) {
+        // For login, we might not need all the posts/comments
+        // Return a basic user object with just username and password
+        final OkHttpClient client = new OkHttpClient().newBuilder().build();
+        final Request request = new Request.Builder()
+                .url(String.format("http://vm003.teach.cs.toronto.edu:20112/user?username=%s", username))
+                .addHeader("Content-Type", CONTENT_TYPE_JSON)
+                .build();
+        try {
+            final Response response = client.newCall(request).execute();
+            final JSONObject responseBody = new JSONObject(response.body().string());
+
+            if (responseBody.getInt(STATUS_CODE_LABEL) == SUCCESS_CODE) {
+                final JSONObject userJSONObject = responseBody.getJSONObject("user");
+                final String name = userJSONObject.getString(USERNAME);
+                final String password = userJSONObject.getString(PASSWORD);
+
+                return userFactory.create(name, password, "", "", "", new ArrayList<>());
+            }
+            else {
+                return null; // User doesn't exist
+            }
+        }
+        catch (IOException | JSONException ex) {
+            return null; // Or throw exception based on your error handling
+        }
+    }
+
+    @Override
+    public void setCurrentUsername(String username) {
+        this.currentUsername = username;
+    }
+
+    @Override
+    public String getCurrentUsername() {
+        return currentUsername;
     }
 
     @Override
@@ -180,4 +240,13 @@ public class DBUserDataAccessObject implements MakePostUserDataAccessInterface,
         }
     }
 
+    @Override
+    public void changePassword(User user) {
+
+    }
+
+    @Override
+    public User findUserByUsername(String username) {
+        return null;
+    }
 }
