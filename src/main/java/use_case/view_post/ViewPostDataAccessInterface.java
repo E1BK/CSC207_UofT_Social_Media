f28fb23d30d90d854e35bc384333a096bc8b5b5d
E1BK <ibekur@gmail.com>
2025-11-22 23:57:15 -0500

    add my name to the top of these files since i worked on them

--- src/main/java/use_case/view_post/ViewPostDataAccessInterface.java
+++ src/main/java/use_case/view_post/ViewPostDataAccessInterface.java
@@ -4,9 +4,5 @@ package use_case.view_post;
 import entity.Post;
 
 public interface ViewPostDataAccessInterface {
-    /**
-     * Return the post with the given postId for the given username,
-     * or null if not found.
-     */
     Post getPost(String username, int postId);
 }
