2388ae2c48a418f73590854dcdd95d6ca13f8f05
HDEnt <dudoeverything@gmail.com>
2025-11-27 21:19:42 -0500

    Clubs API specification and DAO implementation

--- src/main/java/app/AppBuilder.java
+++ src/main/java/app/AppBuilder.java
@@ -1,6 +1,7 @@
 package app;
 
 //import data_access.FileUserDataAccessObject;
+import entity.ClubFactory;
 import interface_adapter.clubs.ClubsController;
 import interface_adapter.clubs.ClubsPresenter;
 import interface_adapter.clubs.ClubsViewModel;
@@ -70,11 +71,12 @@ public class AppBuilder {
     final UserFactory userFactory = new UserFactory();
     final PostFactory postFactory = new PostFactory();
     final CommentFactory commentFactory = new CommentFactory();
+    final ClubFactory clubFactory = new ClubFactory();
     final ViewManagerModel viewManagerModel = new ViewManagerModel();
     public ViewManager viewManager = new ViewManager(cardPanel, cardLayout, viewManagerModel);
 
 
-    final DBUserDataAccessObject userDataAccessObject = new DBUserDataAccessObject(userFactory, postFactory, commentFactory);
+    final DBUserDataAccessObject userDataAccessObject = new DBUserDataAccessObject(userFactory, postFactory, commentFactory, clubFactory);
 
     // Add View Models
     private SignupViewModel signupViewModel;
