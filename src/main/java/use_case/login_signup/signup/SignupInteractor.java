0db7f5200c24e6b0f351be33696592bc8fd8e082
MikeYang533 <mikee.yang@mail.utoronto.ca>
2025-11-30 02:22:15 -0500

    Fix the problem in SignupInteractor

--- src/main/java/use_case/login_signup/signup/SignupInteractor.java
+++ src/main/java/use_case/login_signup/signup/SignupInteractor.java
@@ -46,7 +46,7 @@ public class SignupInteractor implements SignupInputBoundary {
             final User user = userFactory.create(signupInputData.getUsername(), signupInputData.getPassword(), signupInputData.getEmail(), signupInputData.getName());
             userDataAccessObject.save(user);
 
-            final SignupOutputData signupOutputData = new SignupOutputData(user.getName());
+            final SignupOutputData signupOutputData = new SignupOutputData(user.getUsername());
             userPresenter.prepareSuccessView(signupOutputData);
         }
     }
